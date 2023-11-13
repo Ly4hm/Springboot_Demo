8281365c8ad972e395c6a07148a53cdaf673258d
Ly4hm <550653451@qq.com>
2023-11-13 17:47:52 +0800

    增加了Sa-Token, 更新了目录结构

---
+++ src/main/java/com/pj/SaTokenDemoApplication.java
@@ -0,0 +1,16 @@
+package com.pj;
+
+import cn.dev33.satoken.SaManager;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+* 加载安全框架 Sa-Token
+* */
+@SpringBootApplication
+public class SaTokenDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SaTokenDemoApplication.class, args);
+        System.out.println("启动成功，Sa-Token 配置如下：" + SaManager.getConfig());
+    }
+}
