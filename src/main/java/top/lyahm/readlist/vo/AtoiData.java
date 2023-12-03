3dddeb533421834383638afd22ff1e27a96043a7
CR35777 <2404511547@qq.com>
2023-12-03 09:22:58 +0800

    增加了DbData类，返回传感器里的数据

---
+++ src/main/java/top/lyahm/readlist/vo/AtoiData.java
@@ -0,0 +1,13 @@
+package top.lyahm.readlist.vo;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class AtoiData {
+//    数据记录时间
+    private LocalDateTime Date;
+//    AData为三类传感器的数据
+    private float AData;
+}
